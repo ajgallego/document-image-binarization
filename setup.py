ebbb71bf2568186d17862c7c7f1a3f3700487e1f
Javi <ajgallego@gmail.com>
2019-12-18 11:55:42 +0100

    Add setup file

---
+++ setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Installs two executables:
+    - binarize
+    - train
+"""
+import codecs
+
+from setuptools import setup, find_packages
+
+setup(
+    name='document-image-binarization',
+    version='0.1',
+    description='A selectional auto-encoder approach for document image binarization',
+    long_description=codecs.open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    author='Antonio-Javier Gallego, Jorge Calvo-Zaragoza',
+    author_email='jgallego@dlsi.ua.es, jcalvo@dlsi.ua.es',
+    url='https://github.com/ajgallego/document-image-binarization',
+    license='GNU General Public License v3.0',
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=open('requirements.txt').read().split('\n'),
+    package_data={
+        '': ['*.json', '*.yml', '*.yaml'],
+    },
+)
